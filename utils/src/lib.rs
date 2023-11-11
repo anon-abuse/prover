07709ad2e4c890940530c00d47ea2b5abdd54647
cheechyuanang <angchee@live.com.my>
2023-11-12 02:00:01 +0300

    fix bug

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -16,8 +16,29 @@ pub fn add() -> usize {
 //     }
 // }
 
-pub mod transactions;
 pub mod access_list;
 pub mod keccak;
 pub mod signature;
+pub mod transactions;
 pub mod trie;
+
+pub use alloy_rlp as rlp;
+
+pub trait RlpBytes {
+    /// Returns the RLP-encoding.
+    fn to_rlp(&self) -> Vec<u8>;
+}
+
+impl<T> RlpBytes for T
+where
+    T: rlp::Encodable,
+{
+    #[inline]
+    fn to_rlp(&self) -> Vec<u8> {
+        let rlp_length = self.length();
+        let mut out = Vec::with_capacity(rlp_length);
+        self.encode(&mut out);
+        debug_assert_eq!(out.len(), rlp_length);
+        out
+    }
+}
